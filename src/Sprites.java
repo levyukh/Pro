286743f18aa5bc90e8df0f720870ed3c26b8f230
Simon <simonr57@nycstudents.net>
2024-12-10 23:43:58 -0500

    added new enemy sprites

--- src/Sprites.java
+++ src/Sprites.java
@@ -19,7 +19,7 @@ public class Sprites {//used https://kirilllive.github.io/ASCII_Art_Paint/ascii_
                                                                          ████  @ \s
                                                                         ██████   \s
                                                                       @ (# #██   \s
-                                                                        | + |   @\s
+                                                                        | - |   @\s
                                                                       @ \\___|  @ \s
                                                                       ___| |___  \s
                                                                       ▐░░░░░▐░▐  @
@@ -28,8 +28,72 @@ public class Sprites {//used https://kirilllive.github.io/ASCII_Art_Paint/ascii_
                                                                       ▐░░░░░▐░▐ @\s
                                                                       ▐░░░░░▐░▐  \s
                                                                       ▐░░░░░▐░▐  \s
-                                                                      ▐░░░░░▐░▐  \s
     """;
+
+    private static final String annoyingFreshmanSprite = """
+                                                                                 ████  @ \s
+                                                                                ██████   \s
+                                                                              @ (# #██   \s
+                                                                                | - |   @\s
+                                                                              @  ---   @ \s
+                                                                               __| |__   \s
+                                                                              ▐░░░░░▐░▐  @
+                                                                              ▐░░░░░▐░▐  \s
+                                                                              ▐░░░░░▐░▐  \s
+                                                                              ▐░░░░░▐░▐ @\s
+            """;
+    private static final String tiredSeniorSprite = """
+                                                                                 ▓██▒  @ \s
+                                                                                ▓████▓   \s
+                                                                              @ (- -▓▓   \s
+                                                                                | _ |   @\s
+                                                                              @ \\___|  @ \s
+                                                                              ___| |___  \s
+                                                                              ▐░░░░░▐░▐  \s
+                                                                              ▐░░░░░▐░▐  \s
+                                                                              ▐░░░░░▐░▐  \s
+                                                                              ▐░░░░░▐░▐ @
+                                                                              ▐░░░░░▐░▐  \s
+                                                                              ▐░░░░░▐░▐  \s
+                                                                              ▐░░░░░▐░▐  \s
+            """;
+
+    private static final String shadowMillerSprite = """
+                                                                  ,#####,                \s
+                                                                  #_   _#                \s
+                                                                  |a` `a|                \s
+                                                                  |  u  |                \s
+                                                                  \\  =  /                \s
+                                                                  |\\___/|                \s
+                                                         ___ ____/:     :\\____ ___       \s
+                                                       .'   `.-===-\\   /-===-.`   '.     \s
+                                                      /      .-""\"""-.-""\"""-.      \\    \s
+                                                     /'             =:=             '\\   \s
+                                                   .'  ' .:    o   -=:=-   o    :. '  `. \s
+                                                   (.'   /'. '-.....-'-.....-' .'\\   '.) \s
+                                                   /' ._/   ".     --:--     ."   \\_. '\\ \s
+                                                  |  .'|      ".  ---:---  ."      |'.  |\s
+                                                  |  : |       |  ---:---  |       | :  |\s
+                                                   \\ : |       |_____._____|       | : / \s
+                                                   /   (       |----|------|       )   \\ \s
+                                                  /... .|      |    |      |      |. ...\\\s
+                                                 |::::/''     /     |       \\     ''\\::::|
+                                                 '\"\"\"\"       /'    .L_      `\\       \"\"\"\"'
+                                                            /'-.,__/` `\\__..-'\\         \s
+                                                           ;      /     \\      ;         \s
+                                                           :     /       \\     |         \s
+                                                           |    /         \\.   |         \s
+                                                           |`../           |  ,/         \s
+                                                           ( _ )           |  _)         \s
+                                                           |   |           |   |         \s
+                                                           |___|           \\___|         \s
+                                                           :===|            |==|         \s
+                                                            \\  /            |__|         \s
+                                                            /\\/\\           /""\"`8.__     \s
+                                                            |oo|           \\__.//___)    \s
+                                                            |==|                         \s
+                                                            \\__/                         \s
+            """;
 
     public static String getPlayerSprite() {
         return playerSprite;
@@ -38,4 +102,10 @@ public class Sprites {//used https://kirilllive.github.io/ASCII_Art_Paint/ascii_
     public static String getEnemySprite() {
         return enemySprite;
     }
+
+    public static String getAnnoyingFreshmanSprite() {return annoyingFreshmanSprite;}
+
+    public static String getTiredSeniorSprite() {return tiredSeniorSprite;}
+
+    public static String getShadowMillerSprite() {return shadowMillerSprite;}
 }
